70dbd0d26fc36fc05b14878ec0da47a161115dbb
PhilipMay <eniak.info@gmail.com>
2019-09-05 22:42:26 +0200

    config

--- conf.py
+++ conf.py
@@ -80,9 +80,6 @@ source_suffix = {
 
 def setup(app):
     app.add_config_value('recommonmark_config', {
-            #'url_resolver': lambda url: github_doc_root + url,
-            #'auto_toc_tree_section': 'Contents',
-            'enable_auto_toc_tree': True,
             'enable_eval_rst': True,
             }, True)
     app.add_transform(AutoStructify)

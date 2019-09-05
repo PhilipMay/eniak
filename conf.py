480bb0cb5d3a334d5d95518d0555923cd92e51b9
PhilipMay <eniak.info@gmail.com>
2019-09-05 23:03:21 +0200

    css changes

--- conf.py
+++ conf.py
@@ -60,6 +60,12 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = [

125a70c6eccce54cc6ec76abd5ea3fded40e75b4
Philip May <eniak.info@gmail.com>
2020-04-11 10:06:39 +0200

    Create pytorch.py

---
+++ ml/pytorch.py
@@ -0,0 +1,8 @@
+# PyTorch
+
+## Links
+- [PyTorch](https://pytorch.org/)
+  - Docs: https://pytorch.org/docs/stable/index.html
+- Community / Communication
+  - Message Board: https://discuss.pytorch.org/
+
